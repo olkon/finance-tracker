8fedee412065b5fd15df1d86b3b6c54fda06a942
olkon <olli.kontkanen@gmail.com>
2015-12-19 14:34:33 +0000

    stock_quote gem, stock db model and price methods

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151219111202) do
+ActiveRecord::Schema.define(version: 20151219123146) do
+
+  create_table "stocks", force: :cascade do |t|
+    t.string   "ticker"
+    t.string   "name"
+    t.string   "last_price"
+    t.string   "decimal"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
