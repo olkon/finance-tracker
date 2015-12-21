72e270fdd7ece5e11b3da90589143505e6be74b5
olkon <olli.kontkanen@gmail.com>
2015-12-21 18:29:59 +0000

    ajax search basics

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,25 @@
 class UsersController < ApplicationController
   
+  def search
+    @users = User.search(params[:search_param])
+    if @users
+      @users = current_user.except_current_user(@users)
+      render partial 'friends/lookup'
+    else
+      render status: :not_found, nothing: true
+    end
+  end
+  
+  def add_friend
+    @friend = User.find(params[:friend])
+    current_user.friendships.build(friend_id: @friend.id)
+    if current_user.save
+      redirect_to my_friends_path, notice: "You added a new friend!"
+    else
+      redirect_to my_friends_path, notice: "You failed to make a friend!"
+    end
+  end
+  
   def my_portfolio
     @user_stocks = current_user.stocks
     @user = current_user
